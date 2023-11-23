b550a6272b7267821094492b15a45ba3a25fb589
shihab-uddin-shakil <shihabuddinshakil9@gmail.com>
2023-11-23 14:21:27 +0600

    add .gitignore

--- app1/build-test.sh
+++ app1/build-test.sh
@@ -3,15 +3,21 @@ set -ex
 
 PROJECT_ROOT=/home/shihab/Documents/jenkins-workspace/brain-23-app1/app1
 
+#go to project directory
+
+cd $PROJECT_ROOT
+  
+# Build docker app1 image
+
+
 #scan code
+echo "Scanning code...."
 sonar-scanner \
   -Dsonar.projectKey=app1 \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://localhost:9000 \
   -Dsonar.token=sqp_238819299122d7d76f0fd912b18a67cf91d8b117
-  
-# Build docker app1 image
-cd $PROJECT_ROOT
+
 docker build . -t app1 -f ./build/Dockerfile
 
 # Create docker image random tag number
